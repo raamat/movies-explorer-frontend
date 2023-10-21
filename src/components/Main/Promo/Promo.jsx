7cbc68ab6f2b131b1a37d8882506fe5d22c2c6a2
Raamat <raamat@bk.ru>
2023-10-21 21:56:16 +0300

    feat: adapt homepage 768px

--- src/components/Main/Promo/Promo.jsx
+++ src/components/Main/Promo/Promo.jsx
@@ -6,11 +6,11 @@ import promoLogo from "../../../images/promo-logo.svg";
 export default function Promo() {
   return (
     <Section classNameSection="promo">
-      <div className="promo__text">
+      <div className="promo__description">
         <h1 className="promo__title">
           Учебный проект студента факультета Веб&#8209;разработки.
         </h1>
-        <p className="promo__description">
+        <p className="promo__text">
           Листайте ниже, чтобы узнать больше про этот проект и его создателя.
         </p>
         <a className="promo__link" href="#about-project">
