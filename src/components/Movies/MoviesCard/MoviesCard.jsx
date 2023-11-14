781112b8c92273d14310a4c45599637e6a625cb4
Raamat <raamat@bk.ru>
2023-11-14 19:12:48 +0300

    feat: MoviesApi.js

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import "./MoviesCard.css";
 
-const apiURL="https://api.nomoreparties.co/";
+const apiURL = "https://api.nomoreparties.co/";
 
 export default function MoviesCard({ isLike, card }) {
   const [cardLike, setCardLike] = useState(isLike);
@@ -10,11 +10,21 @@ export default function MoviesCard({ isLike, card }) {
     <div className="card">
       <div className="card__title-container">
         <h2 className="card__title">{card.nameRU}</h2>
-        <div className="card__time">{Math. floor(card.duration/60)}ч {card.duration%60}м </div>
+        <div className="card__time">
+          {Math.floor(card.duration / 60)}ч {card.duration % 60}м{" "}
+        </div>
       </div>
-      <img className="card__image" src={apiURL+card.image.url} alt={card.nameRU}></img>
+      <img
+        className="card__image"
+        src={apiURL + card.image.url}
+        alt={card.nameRU}
+      ></img>
       <button
-        className={cardLike ? "card__button card__button_like opacity" : "card__button opacity"}
+        className={
+          cardLike
+            ? "card__button card__button_like opacity"
+            : "card__button opacity"
+        }
         onClick={() => setCardLike(!cardLike)}
       ></button>
     </div>
